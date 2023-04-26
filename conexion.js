79fc3e791f16e973f0cef0fac9c36b1b47f854df
Alan Jimenez <alanjq@outlook.com>
2023-04-25 21:04:47 -0500

    Conexión a la base de datos Librería

---
+++ conexion.js
@@ -0,0 +1,32 @@
+// Conexión a la BD
+let mySql = require('mysql')
+
+// Objeto con datos de la conexión
+const INFO_BD = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'libreria'
+}
+
+let connection = mySql.createConnection(INFO_BD)
+
+let sql = `SELECT id, titulo, clasificacion FROM libreria.libros WHERE clasificacion = 'Programacion'`
+
+// Probar conexión
+connection.connect((error)=>{
+    if(!error){
+        console.log('Conexión exitosa.');
+    }
+    console.error('Falló la conexión.', error);
+})
+
+connection.query(sql, (error, results, fields) => {
+    // Validamos que no haya fallado
+    if (error) {
+        return console.error(error.message)
+    }
+    // Si todo funciona
+    console.log(results);
+})
